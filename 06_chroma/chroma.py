da167841bfca3f3675dcedf9a35cd316060c52a6
xstrecko <xstrecko@fi.muni.cz>
2024-11-27 16:18:03 +0100

    postup

--- 06_chroma/chroma.py
+++ 06_chroma/chroma.py
@@ -7,7 +7,10 @@ collection = client.create_collection("all-my-documents")
 
 # Add docs to the collection. Can also update and delete. Row-based API coming soon!
 collection.add(
-    documents=["This is document1", "This is document2"], # we handle tokenization, embedding, and indexing automatically. You can skip that and add your own embeddings as well
+    documents= [
+        "This is document1", 
+        "This is document2"
+    ], # we handle tokenization, embedding, and indexing automatically. You can skip that and add your own embeddings as well
     metadatas=[{"source": "notion"}, {"source": "google-docs"}], # filter on these!
     ids=["doc1", "doc2"], # unique for each doc
 )
